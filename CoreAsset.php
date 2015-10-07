e987827cd1c2786dd9bf1a7f10eb083ddddfe2fb
norotaro <nelson.otazo@gmail.com>
2015-10-07 15:37:52 -0300

    correccion de error de css

--- CoreAsset.php
+++ CoreAsset.php
@@ -69,7 +69,7 @@ class CoreAsset extends AssetBundle
     public function registerAssetFiles($view)
     {
         $language = $this->language ? $this->language : Yii::$app->language;
-        if ($language != 'en-us' || $language != 'en') 
+        if ($language != 'en-US') 
         {
             $this->js[] = "lang/{$language}.js";
         }
